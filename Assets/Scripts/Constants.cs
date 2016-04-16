b2512473d5c28c4e07db67b8aa7c63c86df5f59a
ermrk <ermrk@seznam.cz>
2016-04-16 11:49:37 +0200

    Game scene created with basic debug interface

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Constants
+{
+    public static int NUMBER_OF_SENSORS = 6;
+}
---
+++ Assets/Scripts/ControllerVisualization/LineVisualization.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineVisualization : MonoBehaviour
+{
+
+    public Vector3 startPosition;
+    public float maxBias;
+    public int[] sensors;
+    public float minValuesToAccept = 0.5f;
+
+    // Use this for initialization
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float nonCenteredValue = 0.0f;
+        float normalizeFactor = getNormalizeFactor();
+        for (int i = 0; i < sensors.Length; i++)
+        {
+            nonCenteredValue += (ControllerValues.GetSensorValueNormalized(sensors[i], minValuesToAccept) / normalizeFactor) * (i + 1) * 100;
+        }
+        Debug.Log(nonCenteredValue);
+        transform.position = new Vector3(getXPosition(nonCenteredValue), startPosition.y, startPosition.z);
+    }
+
+    float getNormalizeFactor()
+    {
+        float totalSensorValue = 0.0f;
+        for (int i = 0; i < sensors.Length; i++)
+        {
+            totalSensorValue += ControllerValues.GetSensorValueNormalized(sensors[i], minValuesToAccept);
+        }
+        if (totalSensorValue == 0)
+        {
+            return 1;
+        }
+        return totalSensorValue;
+    }
+
+    float getXPosition(float nonCenteredValue)
+    {
+        if (nonCenteredValue < 100)
+        {
+            return startPosition.x;
+        }
+        float maxNonCenteredValue = 100 * (sensors.Length - 1);
+        float fraction = (nonCenteredValue - 100) / maxNonCenteredValue;
+        float newPosition = 2 * maxBias * fraction - maxBias;
+        return startPosition.x + newPosition;
+    }
+}
