ad013feeea0d8a76dfdc1ac0dea0b2f977d8d935
ermrk <ermrk@seznam.cz>
2016-04-17 21:32:47 +0200

    Score counter

---
+++ Assets/Score.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    GameObject vehicle;
+    Text scoreText;
+    float score;
+
+	// Use this for initialization
+	void Start () {
+        vehicle = GameObject.FindGameObjectWithTag("Vehicle");
+        scoreText = GameObject.FindGameObjectWithTag("Score").GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        score = Mathf.Clamp(vehicle.transform.position.z-3000,0,float.MaxValue);
+        scoreText.text= "Distance: "+ (int)score+" m";
+    }
+}
